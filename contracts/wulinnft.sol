af2634f0d030d8aab1bb4cf42463ef98f8b1a34b
武林志 <98944616+wulinnet@users.noreply.github.com>
2022-04-02 22:15:42 +0800

    Update wulinnft.sol

--- contracts/wulinnft.sol
+++ contracts/wulinnft.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract WuLin is ERC721Enumerable, Ownable {
+contract WULIN is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
     bool public _isSaleActive = false;
